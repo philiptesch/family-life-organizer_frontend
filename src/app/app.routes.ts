c1dfd3c30a2dd4779d3afc543696abc9f92534d8
philiptesch <philiptesch99@gmail.com>
2026-05-03 17:32:31 +0200

    update router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './main-page/main-page';
+import { Auth } from './auth/auth';
+import { Login } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+     { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '',component: Login },
+
+];
