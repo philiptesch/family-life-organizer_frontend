977fc225874440636ddb45e176b04d3514dd68cd
philiptesch <philiptesch99@gmail.com>
2026-05-07 16:27:17 +0200

    add new component-logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
 import { Auth } from './auth/auth';
 import { Login } from './auth/login/login';
+import { Logo } from './auth/login/logo/logo';
 
 export const routes: Routes = [ 
      { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: '',component: Login },
+    { path: '',component: Logo },
 
 ];
