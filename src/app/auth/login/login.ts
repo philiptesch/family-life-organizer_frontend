32d688743be30e4fc4ef1ebcd4425faac577bd21
Philip <philiptesch99@gmail.com>
2026-05-18 22:00:18 +0200

    add toggle-button to test

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Logo } from './logo/logo';
+import {MatIconModule} from '@angular/material/icon';
 import AOS from 'aos';
 
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, Logo  ],
+  imports: [CommonModule, Logo, MatIconModule  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login implements OnInit {
+isKidMode = false;
 
 
   ngOnInit(): void {
 
     AOS.init()
   }
-
+toggleMode() {
+  this.isKidMode = !this.isKidMode;
+}
 
 }
