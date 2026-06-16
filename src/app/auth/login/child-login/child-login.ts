2a2c4071058655403f1b0443b3a50b3fdb7d0c5e
Philip <philiptesch99@gmail.com>
2026-06-16 21:57:18 +0200

    add login part child

--- src/app/auth/login/child-login/child-login.ts
+++ src/app/auth/login/child-login/child-login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-child-login',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './child-login.html',
   styleUrl: './child-login.scss',
 })
